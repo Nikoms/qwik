38701141f0a8b1920d3b9e7ff35ee65ee9e492aa
Nikoms <nikoms@gmail.com>
2012-12-23 19:33:36 +0100

    Module "Restaurant":  Un menu peut avoir des plats et d'autres menus. Le tout est d'abord filtré en php, afin que la vue ai plus facile

--- kernel/Module/Restaurant/Entity/Restaurant.php
+++ kernel/Module/Restaurant/Entity/Restaurant.php
@@ -8,6 +8,26 @@ use Qwik\Kernel\App\Module\Module;
 class Restaurant extends Module{
 
     public function getTemplateVars(){
-        return parent::getTemplateVars();
+    	$return = parent::getTemplateVars();
+    	$return['children'] =  Restaurant::toTree(parent::getTemplateVars()['menu']);
+        return $return;
+    }
+    
+    static private function toTree($children){
+    	$return = array(
+    		'plats' => array(),
+    		'menu' => array(),
+    	);
+    	foreach($children as $item){
+    		//Par défaut ca va dans "plats"
+    		$where = 'plats';
+    		//S'il a des enfants, alors c'est un menu
+    		if(isset($item['children'])){
+    			$where = 'menu';
+    			$item['children'] = self::toTree($item['children']);
+    		}
+    		$return[$where][] = $item;
+    	}
+    	return $return;
     }
 }
