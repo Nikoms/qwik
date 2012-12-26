14f0fd498bbea80a4631aa8b253ac0f09ff720fa
Nikoms <nikoms@gmail.com>
2012-12-26 03:57:19 +0100

    Seat, accueil pas trop mal + format des prices pour les cartes + todo rajouté pour business

--- kernel/Module/Restaurant/Entity/Restaurant.php
+++ kernel/Module/Restaurant/Entity/Restaurant.php
@@ -9,24 +9,32 @@ class Restaurant extends Module{
 
     public function getTemplateVars(){
     	$return = parent::getTemplateVars();
-    	$return['children'] =  Restaurant::toTree(parent::getTemplateVars()['menu']);
+    	$return['children'] =  $this->toTree(parent::getTemplateVars()['menu']);
         return $return;
     }
     
-    static private function toTree($children){
+    private function toTree($children){
     	$return = array(
     		'plats' => array(),
     		'menu' => array(),
     	);
     	foreach($children as $item){
+
     		//Par défaut ca va dans "plats"
     		$where = 'plats';
     		//S'il a des enfants, alors c'est un menu
     		if(isset($item['children'])){
     			$where = 'menu';
-    			$item['children'] = self::toTree($item['children']);
-    		}
+    			$item['children'] = $this->toTree($item['children']);
+    		}else{
+                $config = $this->getConfig();
+                //Pour formater le prix, si on a un format dans la config
+                if(is_numeric($item['price']) && isset($config['format']) && isset($config['format']['price'])){
+                    $item['price'] = sprintf($config['format']['price'],$item['price']);
+                }
+            }
     		$return[$where][] = $item;
+
     	}
     	return $return;
     }
