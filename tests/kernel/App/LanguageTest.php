8048cf379160d666619b4715719d9efc4880471d
Nikoms <nikoms@gmail.com>
2013-04-18 00:39:44 +0200

    Tests unitaires

---
+++ tests/kernel/App/LanguageTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Nikoms
+ * Date: 2/04/13
+ * Time: 21:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+class LanguageTest extends PHPUnit_Framework_TestCase {
+
+    public static function setUpBeforeClass(){
+        \Qwik\Kernel\App\Language::init(array('fr','nl','en'));
+    }
+
+    public function testInit(){
+        $this->assertSame('fr', \Qwik\Kernel\App\Language::get());
+    }
+
+    public function testGetValueString(){
+        $value = "ceci est un test";
+        $this->assertSame($value, \Qwik\Kernel\App\Language::getValue($value));
+    }
+
+    public function testGetValueArrayOk(){
+        $value = array(
+            'nl' => "ceci est un test (nl)",
+            'fr' => "ceci est un test (fr)",
+        );
+        $this->assertSame("ceci est un test (fr)", \Qwik\Kernel\App\Language::getValue($value));
+    }
+
+    public function testGetValueArrayOnlyTaken(){
+        $value = array(
+            'nl' => "ceci est un test (nl)",
+        );
+        $this->assertSame("ceci est un test (nl)", \Qwik\Kernel\App\Language::getValue($value));
+    }
+
+    public function testGetValueArrayOnlyGoodTaken(){
+        $value = array(
+            'fr' => "ceci est un test (fr)",
+        );
+        $this->assertSame("ceci est un test (fr)", \Qwik\Kernel\App\Language::getValue($value));
+    }
+
+    public function testChangeIfPossible(){
+        \Qwik\Kernel\App\Language::changeIfPossible('nl');
+        $this->assertSame('nl', \Qwik\Kernel\App\Language::get());
+    }
+
+    public function testGetValueArrayFirstTaken(){
+        $value = array(
+            'fr' => "ceci est un test (fr)",
+            'en' => "ceci est un test (en)",
+        );
+        $this->assertSame("ceci est un test (fr)", \Qwik\Kernel\App\Language::getValue($value));
+    }
+}
